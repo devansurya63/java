9112922fa599efca888d73bec703aecdffe54253
devansurya63 <devansurya295@gmail.com>
2024-12-13 11:30:39 +0530

    Add files via upload
    12/12/2024 task.

---
+++ MirrorRighttriangle.java
@@ -0,0 +1,17 @@
+package javaprogram;
+
+public class MirrorRighttriangle {
+
+    public static void main(String[] args) {
+        int rows = 10;
+
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= rows - i; j++) 
+                System.out.print(" ");    
+            for (int j = 1; j <= i; j++) 
+                System.out.print("*");
+            
+            System.out.println(); 
+        }
+    }
+}
